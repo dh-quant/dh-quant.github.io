dd998448320159e547e1d222ae7241eacf848cd0
dh-quant <dh-quant@users.noreply.github.com>
2026-04-16 18:02:59 +0000

    Deploying to gh-pages from @ dh-quant/dh-quant.github.io@cd56f7a428c970da28e5c46f28ca749232744083 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dh-quant/donghyeok-kim.github.io",
+    "data-repo": "dh-quant/dh-quant.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
